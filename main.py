4f8f39c69611016302e2d8f3a24de9844583c465
Bdelas777 <93608793+Bdelas777@users.noreply.github.com>
2023-05-08 12:06:31 -0600

    Enemigos

--- main.py
+++ main.py
@@ -39,22 +39,58 @@ class Jugador (pygame.sprite.Sprite):
         if self.rect.left < 0:
             self.rect.left = 0
 
+class Enemigo(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("assets/meteorGrey_med1.png").convert()
+        self.image.set_colorkey(NEGRO)
+        self.rect = self.image.get_rect()
+        self.rect.y = random.randrange(-100, -40)
+        self.speedy = random.randrange(1, 10)
+        self.speedx = random.randrange(-5, 5)
+        
+    def update(self):
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        if self.rect.top > LARGO+ 10 or self.rect.left < -25 or self.rect.right > ANCHO + 22 :
+            self.rect.x = random.randrange(ANCHO - self.rect.width)
+            self.rect.y = random.randrange(-100, -40)
+            self.speedy = random.randrange(1, 8)
+ 
+ # Cargar fondo.
+background = pygame.image.load("assets/background.png").convert()
+           
 all_sprites = pygame.sprite.Group()
+enemigoLista = pygame.sprite.Group()
 
 jugador = Jugador()
 
 all_sprites.add(jugador)
 
 # Correr todo el juego
+for i in range(8):
+	enemigo = Enemigo()
+	all_sprites.add(enemigo)
+	enemigoLista.add(enemigo)
+
+# Game Loop
+running = True
 while running:
-    reloj.tick(60)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-            
-    all_sprites.update()
-    screen.fill(NEGRO)
-    all_sprites.draw(screen)
-    pygame.display.flip()
+	# Keep loop running at the right speed
+	reloj.tick(60)
+	# Process input (events)
+	for event in pygame.event.get():
+		# check for closing window
+		if event.type == pygame.QUIT:
+			running = False
+		
+	# Update
+	all_sprites.update()
+
+	#Draw / Render
+	screen.blit(background, [0, 0])
+	all_sprites.draw(screen)
+	# *after* drawing everything, flip the display.
+	pygame.display.flip()
 
 pygame.quit()
