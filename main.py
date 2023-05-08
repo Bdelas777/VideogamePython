b210aa9491bd88a016123079cf4b0993d081b277
Bdelas777 <93608793+Bdelas777@users.noreply.github.com>
2023-05-08 11:50:49 -0600

    Menu

--- main.py
+++ main.py
@@ -19,15 +19,25 @@ running = True
 class Jugador (pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("assets/player.png")
-        self.image.set_colorkey("NEGRO")
-        self.rect = self.rect.image.get_rect()
+        self.image = pygame.image.load("assets/player.png").convert()
+        self.image.set_colorkey(NEGRO)
+        self.rect = self.image.get_rect()
         self.rect.centerx = ANCHO // 2
         self.rect.bottom = LARGO - 10
         self.speed_x = 0
         
     def update(self):
-        pass
+        self.speed_x = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.speed_x = -5
+        if keystate[pygame.K_RIGHT]:
+            self.speed_x = 5
+        self.rect.x += self.speed_x
+        if self.rect.right > ANCHO:
+            self.rect.right = ANCHO
+        if self.rect.left < 0:
+            self.rect.left = 0
 
 all_sprites = pygame.sprite.Group()
 
@@ -35,6 +45,16 @@ jugador = Jugador()
 
 all_sprites.add(jugador)
 
-
+# Correr todo el juego
 while running:
     reloj.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            
+    all_sprites.update()
+    screen.fill(NEGRO)
+    all_sprites.draw(screen)
+    pygame.display.flip()
+
+pygame.quit()
